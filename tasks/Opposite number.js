43a9b93770eb977057e2f19b6182f6b288ac6e5d
Serhii <yakovenko.sergey.work@gmail.com>
2022-07-20 17:34:04 +0300

    task | opposite number

---
+++ tasks/Opposite number.js
@@ -0,0 +1,9 @@
+function opposite(number) {
+   function opposite(number) {
+      let num = number ? -number : number;
+      return num;
+   };
+}
+console.log(opposite(34.245))
+console.log(opposite(-34.245))
+console.log(opposite(-2));

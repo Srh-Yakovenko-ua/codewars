1a068eab745b87ff8565a9744ddbae47ad44f1a5
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-07-22 16:02:48 +0300

    task | basic mathematical operations

---
+++ tasks/Basic Mathematical Operations.js
@@ -0,0 +1,27 @@
+function basicOp(operation, value1, value2) {
+    if(operation === '+') return value1 + value2;
+        if(operation === '-') return value1 - value2;
+        if(operation === '*') return value1 * value2;
+        if(operation === '/') return value1 / value2;
+
+//    switch (operation) {
+//         case '+':
+//             return value1 + value2;
+//             break;
+//         case '-':
+//             return value1 - value2;
+//             break;
+//         case "*":
+//             return value1 * value2;
+//             break;
+//         case "/":
+//             return value1 / value2;
+//             break;
+//
+//     }
+ }
+
+console.log(basicOp('+', 4, 7))
+console.log(basicOp('-', 15, 18))
+console.log(basicOp('*', 5, 5))
+console.log(basicOp('/', 49, 7))

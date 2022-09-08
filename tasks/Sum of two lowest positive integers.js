04046996b814f8118d0e905cb6a4117da1b6d52f
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-09-08 21:50:26 +0300

    task | sum of to lowest

---
+++ tasks/Sum of two lowest positive integers.js
@@ -0,0 +1,24 @@
+function sumTwoSmallestNumbers(numbers) {
+    let value = 0
+    for(let i = 0;i < numbers.length;i++){
+        for (let j = 0; j <numbers.length ; j++) {
+            if(numbers[j] > numbers[j + 1]){
+                value = numbers[j];
+                numbers[j] = numbers[j+ 1] ;
+                numbers[j+1] = value
+            }
+        }
+
+    }
+    return numbers[0] + numbers[1]
+
+}
+
+console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]))
+
+// function sumTwoSmallestNumbers(numbers) {
+//   let [firstMin,secondMin] = numbers.sort((a,b) => a - b)
+//     return firstMin + secondMin
+// }
+
+console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]))

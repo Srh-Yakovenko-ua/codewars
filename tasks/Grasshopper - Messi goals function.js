9e5bbc1efc62de8eb45bb169608c6bd716e04d68
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-09-08 21:28:19 +0300

    task | messi goals function

---
+++ tasks/Grasshopper - Messi goals function.js
@@ -0,0 +1,11 @@
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
+
+console.log(goals(43, 10, 5))
+
+
+// function goals () {
+//     return arguments[0] + arguments[1] + arguments[2]
+// }
+// console.log(goals(43, 10, 5))

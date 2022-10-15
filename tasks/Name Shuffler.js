f2c57156cd7cf5f838f70844984b08cf727b27a5
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-10-15 19:08:23 +0300

    task | Name Shuffler

---
+++ tasks/Name Shuffler.js
@@ -0,0 +1,7 @@
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+}
+
+console.log(nameShuffler('john McClane'))
+console.log(nameShuffler('Mary jeggins'))
+console.log(nameShuffler('tom jerry'))

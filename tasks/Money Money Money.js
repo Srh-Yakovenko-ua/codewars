be4ad258685269993c4f0528041814128070e4bc
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-11-11 23:21:44 +0300

    task |MONEY MONEY MONEY

---
+++ tasks/Money Money Money.js
@@ -0,0 +1,11 @@
+function calculateYears(principal, interest, tax, desired) {
+    let years = 0;
+    while (principal < desired) {
+        principal += principal * interest * (1 - tax);
+        years++;
+    }
+    return years;
+}
+
+console.log(calculateYears(1000, 0.05, 0.18, 1100))
+console.log(calculateYears(1000,0.01625,0.18,1200))

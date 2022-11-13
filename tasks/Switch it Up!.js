2e1635057eb27f0db7f59f2e9b513ea27c86b69c
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-11-13 19:41:04 +0300

    task |Switch it Up!

---
+++ tasks/Switch it Up!.js
@@ -0,0 +1,19 @@
+function switchItUp(number) {
+    switch (number) {
+        case 0: return 'Zero';
+        case 1: return 'One';
+        case 2: return 'Two';
+        case 3: return 'Three';
+        case 4: return 'Four';
+        case 5: return 'Five';
+        case 6: return 'Six';
+        case 7: return 'Seven';
+        case 8: return 'Eight';
+        case 9: return 'Nine';
+    }
+}
+
+console.log(switchItUp(1))
+console.log(switchItUp(3))
+console.log(switchItUp(5))
+console.log(switchItUp(9))

7fac50ff4b33a2d3bed9a8e068f714ae00cfb4b2
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-12-27 23:52:50 +0300

    task | Isograms

---
+++ tasks/Isograms.js
@@ -0,0 +1,12 @@
+function isIsogram(str) {
+    const strArr = str.toLowerCase().split('').sort();
+
+    for (let i = 0; i < strArr.length - 1; i++) {
+        if (strArr[i] === strArr[i + 1]) {
+            return false;
+        }
+    }
+    return  true
+}
+
+console.log(isIsogram('moOse'))

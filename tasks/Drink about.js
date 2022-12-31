43a82ff00905dd462037b35a1653df7e1d1dbecb
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-12-31 23:56:44 +0300

    task | Drink about

--- tasks/Drink about.js
+++ tasks/Drink about.js
@@ -1,12 +1,12 @@
 function peopleWithAgeDrink(old) {
-    if(old < 14){
-        return ("drink toddy");
-    } else if((old < 18) && (old >= 14)){
-        return ("drink coke");
-    } else if((old < 21) && (old >= 18)){
-        return ("drink beer");
-    } else if(old >= 21){
-        return ("drink whisky");
+    if (old < 14) {
+        return 'drink toddy';
+    } else if (old < 18) {
+        return 'drink coke';
+    } else if (old < 21) {
+        return 'drink beer';
+    } else {
+        return 'drink whisky';
     }
 }
 

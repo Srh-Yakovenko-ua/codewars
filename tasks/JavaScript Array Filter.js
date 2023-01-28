ea39d98b257ddf968c4fd6666cbd926d840626b7
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-01-28 22:01:19 +0300

    task | JavaScript Array Filter

---
+++ tasks/JavaScript Array Filter.js
@@ -0,0 +1,8 @@
+function getEvenNumbers(numbersArray){
+    return numbersArray.filter(el => el % 2 ===0)
+}
+
+console.log(getEvenNumbers([1,2,3,6,8,10])) // [2,6,8,10])
+console.log(getEvenNumbers([1,2])) //[2]
+console.log(getEvenNumbers([12,14,15])) //[12,14]
+

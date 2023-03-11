88b1fbc2d21c1cee39271a4eeb4ffddec3d1c27c
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-03-11 13:19:54 +0300

    task | Disemvowel Trolls

--- tasks/Disemvowel Trolls.js
+++ tasks/Disemvowel Trolls.js
@@ -1,7 +1,15 @@
+// function disemvowel(str) {
+//     const vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+//     return str.split('').filter(letter => !vowels.includes(letter.toLowerCase())).join('')
+//
+// }
 function disemvowel(str) {
-    const vowels = ['a', 'e', 'i', 'o', 'u', 'y']
-    return str.split('').filter(letter => !vowels.includes(letter.toLowerCase())).join('')
-
+    let newStr = '';
+    for (let i = 0; i < str.length; i++) {
+        if ('aeiou'.includes(str[i].toLowerCase())) continue;
+        newStr += str[i];
+    }
+    return newStr;
 }
 
 console.log(disemvowel('This website is for losers LOL!'))

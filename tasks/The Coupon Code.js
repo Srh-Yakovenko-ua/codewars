84391d783b6e935fdacfe04d3938af5c6b566dd2
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-03-17 21:40:26 +0300

    task | The Coupon Code

---
+++ tasks/The Coupon Code.js
@@ -0,0 +1,6 @@
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+
+    return enteredCode === correctCode && Date.parse(currentDate) <= Date.parse(expirationDate)
+}
+
+console.log(checkCoupon('123','123','September 5, 2014','October 1, 2014')) //true
